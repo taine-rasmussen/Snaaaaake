f75f72a9250c2592acd35525250817f77dfa09d4
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-01-29 19:49:54 +0000

    set up game constants

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { 
+  CANVAS_SIZE,
+  SPEED,
+  SNAKE_START,
+  FRUIT_START 
+} from './Constants'
 
 function App() {
   return (
     <div className="App">
-      Snake
+      snake
     </div>
   );
 }
---
+++ src/Constants.js
@@ -0,0 +1,11 @@
+const CANVAS_SIZE = [800, 800];
+const SPEED = 250;
+const SNAKE_START = [8, 5]
+const FRUIT_START = [2, 4]
+
+export {
+  CANVAS_SIZE,
+  SNAKE_START,
+  FRUIT_START,
+  SPEED
+}

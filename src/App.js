7a8f26f30f470382c3b0f2857dd20fb490e36754
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-01-29 20:06:08 +0000

    center gameboard

--- src/App.js
+++ src/App.js
@@ -8,8 +8,15 @@ import {
 
 function App() {
   return (
-    <div className="App">
-      snake
+    <div className="app_container">
+      <canvas
+        style={{ border: "1px solid black" }}
+        width={`${CANVAS_SIZE[0]}px`}
+        height={`${CANVAS_SIZE[1]}px`}
+      />
+      <button>
+        Start game
+      </button>
     </div>
   );
 }

84f97909f78514ad34f326763a5a55d0ce7ebc19
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-01-29 20:38:05 +0000

    assign div role to btn

--- src/App.js
+++ src/App.js
@@ -42,13 +42,19 @@ function App() {
   )
 
   return (
-    <div className="app_container">
+    <div 
+      className="app_container"
+      role="button"
+      tabIndex="0"
+    >
       <canvas
         style={{ border: "1px solid black" }}
         width={`${CANVAS_SIZE[0]}px`}
         height={`${CANVAS_SIZE[1]}px`}
       />
-      <button>
+      <button
+        onClick={startGame}
+      >
         Start game
       </button>
     </div>
